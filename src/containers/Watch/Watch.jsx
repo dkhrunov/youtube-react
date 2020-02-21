847b3e57742b744c1d453e92622088903ace3ee2
therealpanda98 <therealpanda@mail.ru>
2020-02-22 00:51:25 +0300

    Создан компонент Video для просмотра видео из YouTube.

--- src/containers/Watch/Watch.jsx
+++ src/containers/Watch/Watch.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './Watch.scss';
+import Video from '../../components/Video/Video';
 
 const Watch = (props) => {
-	return <div>Watch component</div>;
+	return (
+		<div style={{ maxWidth: '80%' }}>
+			<Video id='-7fuHEEmEjs' />
+		</div>
+	);
 };
 
 export default Watch;
